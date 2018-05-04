aeefefbf4075edf29e882ec931b5bf534b3d84ad
KING SABRI <king.sabri@gmail.com>
2018-05-04 11:22:22 +0300

    enhace terminal tabel output

--- lib/osa/report/terminal.rb
+++ lib/osa/report/terminal.rb
@@ -60,7 +60,7 @@ module OSA
               group << rule.group
             end
           end
-
+          
           rows << [ name, status, public_addr.join("\n"), private_addr.join("\n"),
                     sec_grp.join("\n"), protocol.join("\n"), from_port.join("\n"),
                     to_port.join("\n"), ip_range.join("\n"), group.join("\n") ]
